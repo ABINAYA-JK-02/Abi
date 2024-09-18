76dfc274abc348dfc03c7c68555ffa96895bc7e2
Your Name <your.email@example.com>
2024-09-18 19:03:18 +0530

    Abi

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+// import Footer from "./components/Footer";
+import Navbar from "./components/NavBar";
+
+const App = () => {
+    const appname = "Nav bar";
+
+    return (
+        <>
+            <div className="h-screen w-screen flex flex-col justify-start items-center">
+                <Navbar appname={appname} />
+            </div>
+
+        </>
+    )
+}
+
+export default App;
---
+++ portfolio/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+const Navbar = ({ Appname }) => {
+
+    console.log(appname)
+    return (
+        <div className="w-full h-[3vh] grad-blue-bg text-white flex justify-center items-center gap-4">
+            {appname}
+        </div>
+    )
+}
+
+export default Navbar;
